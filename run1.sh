73ed7d31d8c69529663f88de1368fad0fa6d0007
cubotv68 <86035392+cubotv68@users.noreply.github.com>
2021-07-11 21:45:33 +0700

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,1 +1,1 @@
-./ethminer --farm-recheck 999 -U -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@us2.ethermine.org:5555
+./ethminer --farm-recheck 999 -U -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@us2.ethermine.org:5555
