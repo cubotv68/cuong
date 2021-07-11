31d25d1b9088b3ae6c6ae2b8366fd4988927cd76
cubotv68 <86035392+cubotv68@users.noreply.github.com>
2021-07-11 21:45:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 ./ethminer --farm-recheck 400 -U \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@eu1.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@us1.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@eu2.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@us2.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h2@asia1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@eu1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@us1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@eu2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@us2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xc1B7b08fe2F66e5d0f4434266d133Eb55BE689c6.h2@asia1.ethermine.org:5555 \
                     	
